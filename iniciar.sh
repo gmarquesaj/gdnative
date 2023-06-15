59df7e173a68d43a6032e84d98a144f398c1efe3
gmarquesaj <gmarquesaj@gmail.com>
2023-06-15 01:43:31 -0300

    copiar arquivo padrao antes de trocar de pasta

--- iniciar.sh
+++ iniciar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp padrao.cpp $2/$1.cpp
 cd $2
 printf '[gd_resource type="GDNativeLibrary" format=2]\n\n[resource]\nentry/X11.64 = "res://SimpleLibrary/bin/lib_'>lib.tres
 printf $1>>lib.tres
@@ -7,11 +8,10 @@ printf '.so"\ndependency/X11.64 = [  ]'>>lib.tres
 printf '[gd_resource type="NativeScript" load_steps=2 format=2]\n[ext_resource path="res://lib.tres" type="GDNativeLibrary" id=1]\n\n[resource]\nclass_name = "'>lib.gdns
 printf $1 >>lib.gdns
 printf '"\nlibrary = ExtResource( 1 )'>>lib.gdns
-cp padrao.cpp $1.cpp
 sed -i.bak 's|SimpleClass|'${1}'|g' $1.cpp
-#git init
-#git add .
-#git commit -am "inicio"
+git init
+git add .
+git commit -am "inicio"
 mkdir SimpleLibrary
 cd SimpleLibrary
 mkdir bin
@@ -20,11 +20,11 @@ mkdir src
 mv ../$1.cpp src/
 git submodule add -b 3.4 https://github.com/godotengine/godot-cpp
 git submodule update --init --recursive
-git clone --recursive https://github.com/godotengine/godot-cpp -b 3.4
+git clone --recursive https://github.com/godotengine/godot-cpp -b 3.5
 
 
 cd godot-cpp
-scons generate_bindings=yes -j8
+scons generate_bindings=yes -j16
 cd ..
 
 clang++ -fPIC -o src/$1.o -c src/$1.cpp -g -O3 -std=c++14 -Igodot-cpp/include -Igodot-cpp/include/core -Igodot-cpp/include/gen -Igodot-cpp/godot-headers -pthread
